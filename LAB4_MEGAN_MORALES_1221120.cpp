8573c1446a81e830760df277ab0da11d843c06c1
Megan-Morales <63917946+Megan-Morales@users.noreply.github.com>
2021-09-19 14:59:50 -0600

    Crear la estructura del posiciones del mapa

--- LAB4_MEGAN_MORALES_1221120.cpp
+++ LAB4_MEGAN_MORALES_1221120.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+int num_Nodos = 1;
+
+struct nodo {
+	int id_Posicion;
+	bool conejo_en_Nodo = false;
+	bool liebre_en_Nodo = false;
+	nodo* siguiente;
+	nodo* atras;
+}*primero, *ultimo;
+
+
+
+
 
 
 int main() {
-	cout << "Hola";
+	
+
 }
 
 
